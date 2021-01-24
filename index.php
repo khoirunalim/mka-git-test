5baae3f2dcce6c082e28b4bba3eae74f806b14f5
khoirunalim <khoirunalim24@gmail.com>
2021-01-24 21:50:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <p>isinya paragraph</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, architecto sunt ducimus aperiam, officia praesentium accusamus ad soluta odio, tenetur culpa aliquid voluptatem sed consequuntur dolorum quasi ea. Quas, assumenda?</p>
+    <ul></ul>
+    <li>List1</li>
+    <li>List2</li>
+    </ul>
 </body>
 
 </html>
